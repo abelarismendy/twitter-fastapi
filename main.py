7dfa16ad5908b551e17cafdbea1a8cf1548073c5
Abel Arismendy <abel@arismendy.co>
2022-07-10 14:51:21 -0500

    feat(Tweet): Tweet base model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 # Pydantic
@@ -21,6 +21,7 @@ class UserLogin(UserBase):
     password : str = Field(
         ...,
         min_length=8,
+        max_length=64,
     )
 
 class User(UserBase):
@@ -37,7 +38,15 @@ class User(UserBase):
     birth_date : Optional[date] = Field(default=None)
 
 class Tweet(BaseModel):
-    pass
+    tweet_id : UUID = Field(...)
+    user_id : UUID = Field(...)
+    content : str = Field(
+        ...,
+        min_length=1,
+        max_length=256,
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at : Optional[datetime] = Field(default=None)
 
 @app.get(
     path="/",
